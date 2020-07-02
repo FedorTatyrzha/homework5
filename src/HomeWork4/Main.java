3e7c709b06ac640f465beaaa2968a0379354d1f0
ponchh18 <67115645+ponchh18@users.noreply.github.com>
2020-07-02 14:24:30 +0300

    1

---
+++ src/HomeWork4/Main.java
@@ -0,0 +1,9 @@
+package HomeWork4;
+
+public class Main {
+    public static void main(String[] args) {
+        Compucter cp1 = new Compucter();
+        cp1.printCompInformation();
+        cp1.metod();
+    }
+}
